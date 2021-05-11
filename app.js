3b5241fc9fe488c97bdcb550c855584ea15e43e7
Ziglar1 <bas@dejong.pro>
2021-05-11 15:12:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ class MyApp extends Homey.App {
     const secret = Homey.env.WEBHOOK_SECRET; // "2uhf83h83h4gg34..."
     const data = {
       // Provide unique properties for this Homey here
-      deviceId: 'ispindel001',
+      deviceId: 'Ispindel001',
+      name: ''
     };
 
     const myWebhook = await this.homey.cloud.createWebhook(id, secret, data);
 
     myWebhook.on('message', args => {
-      this.log('Got a webhook message!');
-      this.log('headers:', args.headers);
-      this.log('query:', args.query);
-      this.log('body:', args.body);
+      console.log(args)
     });
   }
 }
