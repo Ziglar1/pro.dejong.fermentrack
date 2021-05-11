119e53dc3df5f59e866b2460b62346b9a411668a
Ziglar1 <bas@dejong.pro>
2021-05-11 14:49:15 +0200

    webhook

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const Homey = require('homey');
 
 class MyApp extends Homey.App {
-  /**
-   * onInit is called when the app is initialized.
-   */
   async onInit() {
-    this.log('MyApp has been initialized');
-	const card = this.homey.flow.getTriggerCard('PushIn');
+    const id = Homey.env.WEBHOOK_ID; // "56db7fb12dcf75604ea7977d"
+    const secret = Homey.env.WEBHOOK_SECRET; // "2uhf83h83h4gg34..."
+    const data = {
+      // Provide unique properties for this Homey here
+      deviceId: 'ispindel001',
+    };
+
+    const myWebhook = await this.homey.cloud.createWebhook(id, secret, data);
+
+    myWebhook.on('message', args => {
+      this.log('Got a webhook message!');
+      this.log('headers:', args.headers);
+      this.log('query:', args.query);
+      this.log('body:', args.body);
+    });
   }
 }
 
